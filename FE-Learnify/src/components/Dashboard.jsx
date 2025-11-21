9a6e0be985476bee44780f64b74810a74528ab61
Althaf-Budiman <budimanaltaf687@gmail.com>
2025-11-21 15:28:26 +0700

    init shadcn ui

--- FE-Learnify/src/components/Dashboard.jsx
+++ FE-Learnify/src/components/Dashboard.jsx
@@ -1,3 +1,5 @@
+import { Button } from "./ui/button";
+
 export default function Dashboard() {
     return (
         <div className="flex-1 flex flex-col bg-gray-100">
@@ -10,6 +12,7 @@ const Header = () => {
     return (
         <header className="py-10 px-12 flex items-center justify-between">
             <h1 className="text-2xl font-bold text-gray-800">List Kelas</h1>
+            <Button className="w-fit">Test</Button>
         </header>
     )
 }
