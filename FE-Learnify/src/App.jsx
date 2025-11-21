3472ab1f940b96e7adc5ec4dae61ccd39531c384
Althaf-Budiman <budimanaltaf687@gmail.com>
2025-11-21 15:03:23 +0700

    init main content

--- FE-Learnify/src/App.jsx
+++ FE-Learnify/src/App.jsx
@@ -1,15 +1,14 @@
 import './App.css'
+import Dashboard from './components/Dashboard'
 import Sidebar from './components/Sidebar'
 
 function App() {
-  
 
   return (
-    <>
+    <div className="flex h-screen">
       <Sidebar />
-
-     
-    </>
+      <Dashboard />
+    </div>
   )
 }
 
--- FE-Learnify/src/components/Dashboard.jsx
+++ FE-Learnify/src/components/Dashboard.jsx
@@ -1,9 +1,15 @@
-function app(){
-    return(
-        <>
-            skdhsdskhd
-        </>
+export default function Dashboard() {
+    return (
+        <div className="flex-1 flex flex-col bg-gray-100">
+            <Header />
+        </div>
     );
 }
 
-export default app;
+const Header = () => {
+    return (
+        <header className="py-10 px-12 flex items-center justify-between">
+            <h1 className="text-2xl font-bold text-gray-800">List Kelas</h1>
+        </header>
+    )
+}
