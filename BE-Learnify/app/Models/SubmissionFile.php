527b9b99db1d1cf8cc4964cb400253db1bb08b25
DrelezTM <dreleztm@gmail.com>
2025-11-25 14:39:46 +0700

    feat: fixed assignment, submission and files submission

--- BE-Learnify/app/Models/SubmissionFile.php
+++ BE-Learnify/app/Models/SubmissionFile.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class SubmissionFile extends Model
 {
-    //
+    protected $fillable = [
+        'submission_id',
+        'file_path',
+    ];
+
+    public function assignmentSubmission() {
+        return $this->belongsTo(AssignmentSubmission::class, 'submission_id');
+    }
 }
